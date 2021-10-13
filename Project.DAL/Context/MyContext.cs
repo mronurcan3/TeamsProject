16fa1bd31151ee04e24a0256cebd545ad5ce3ffd
naciyeceren097 <naciye.ceren097@gmail.com>
2021-10-13 23:24:29 +0300

    adding DAL and BLL layers

---
+++ Project.DAL/Context/MyContext.cs
@@ -0,0 +1,37 @@
+﻿using Project.Entities.Models;
+using Project.MAP.Options;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Context
+{
+    public class MyContext:DbContext
+    {
+        public MyContext():base("MyConnection")
+        {
+            
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AppUserMap());
+            modelBuilder.Configurations.Add(new UserProfileMap());
+            modelBuilder.Configurations.Add(new ProductMap());
+            modelBuilder.Configurations.Add(new CategoryMap());
+            modelBuilder.Configurations.Add(new OrderMap());
+            modelBuilder.Configurations.Add(new OrderDetailMap());
+            modelBuilder.Configurations.Add(new ShipperMap());
+        }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<UserProfile> Profiles { get; set; }
+        public DbSet<Shipper> Shippers { get; set; }
+    }
+}
